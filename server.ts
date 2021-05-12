a3f49280111bc29e4586f2e38e743782a2b39216
Javad Shafique <javadshafique@hotmail.com>
2021-05-12 10:15:46 +0200

    Initial commit

---
+++ server.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import * as http from 'http';
+import * as path from 'path';
+import * as WebSocket from 'ws';
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let currentState: Record<string, any> = {};
+
+wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', (message: string) => {
+        console.log('received: %s', (message));
+        // Convert json string into json object
+        const data: Record<string, any> = JSON.parse(message);
+
+
+        if (data.type === 'position' || Object.keys(data).includes('coords')) {
+            currentState[data.id] = data.coords;
+            console.log('Updated coords for %s', (data.id));
+        } else if (data.type === 'fetch') {
+            if (Object.keys(currentState).includes(data.id)) {
+                ws.send(currentState[data.id]);
+            }
+        } else if (data.type === 'all') {
+            ws.send(JSON.stringify(currentState));
+        }
+    });
+
+    ws.send('ready');
+});
+
+// Serve index file with express
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+// Start server
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port ${(server.address() as WebSocket.AddressInfo).port}`);
+});
